0fafa40975fc39f3e036e7854e9d9067264f9ce0
gdp <gdp@zigzag.ints.net>
2022-08-25 18:41:58 +0300

    rename test

--- src/test/java/com/example/rickandmorty/RickAndMortyApplicationTest.java
+++ src/test/java/com/example/rickandmorty/RickAndMortyApplicationTest.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RickandmortyApplicationTests {
-
+class RickAndMortyApplicationTest {
     @Test
     void contextLoads() {
     }
-
 }
